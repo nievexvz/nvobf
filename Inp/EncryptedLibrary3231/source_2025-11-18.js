c466a7c3f27980d3faf57cca166aef05bb5c9051
NineTwelve <ninetwelvefirstqy12@gmail.com>
2025-11-19 04:47:28 +0700

    Add source code for EncryptedLibrary3231

---
+++ Inp/EncryptedLibrary3231/source_2025-11-18.js
@@ -0,0 +1,30 @@
+// Welcome to JavaScript Obfuscator!
+// Paste your JavaScript code here and click "Process Obfuscation"
+
+function calculateSum(a, b) {
+    const result = a + b;
+    console.log('The sum is:', result);
+    return result;
+}
+
+const multiply = (x, y) => {
+    return x * y;
+};
+
+class Calculator {
+    constructor() {
+        this.history = [];
+    }
+    
+    add(a, b) {
+        const result = a + b;
+        this.history.push(`${a} + ${b} = ${result}`);
+        return result;
+    }
+}
+
+// Example usage
+const calc = new Calculator();
+
+calc.add(5, 3);
+calculateSum(10, 20);
